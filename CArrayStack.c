dc245eb33665c8c3ecda0f49b2fb4bfb9b25142e
FunctionGHW <functionghw@hotmail.com>
2013-04-17 15:23:23 +0800

    Create the Project
    ver 0.1

---
+++ CArrayStack.c
@@ -0,0 +1,10 @@
+/* Project:     ArrayStack in C
+ * File:        CArrayStack.c
+ * Coder:       FunctionGHW
+ * Version:     0.1
+ * Last Change: 2013-4-17
+ * Description: Stack for C implemented using array.
+ *              This is an inmplementation of CArrayStack.h.
+ */
+
+#include "CArrayStack.h"
---
+++ CArrayStack.h
@@ -0,0 +1,15 @@
+/* Project:     ArrayStack in C
+ * File:        CArrayStack.h
+ * Coder:       FunctionGHW
+ * Version:     0.1
+ * Last Change: 2013-4-17
+ * Description: Stack for C implemented using array.
+ *              This is the header file of arraystack.
+ */
+
+#ifndef _CARRAYSTACK_E1AFBBBE_7627_4C18_8F56_72348535ED6D
+#define _CARRAYSTACK_E1AFBBBE_7627_4C18_8F56_72348535ED6D
+
+
+#endif
+// _CARRAYSTACK_E1AFBBBE_7627_4C18_8F56_72348535ED6D
